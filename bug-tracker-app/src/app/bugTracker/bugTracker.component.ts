0089d01174be4fa670f77fc68c1a150fe6de1b32
Magesh <tkmagesh77@gmail.com>
2018-04-01 16:39:20 +0530

    Day-02 Complete

--- bug-tracker-app/src/app/bugTracker/bugTracker.component.ts
+++ bug-tracker-app/src/app/bugTracker/bugTracker.component.ts
@@ -19,4 +19,20 @@ export class BugTrackerComponent{
 	onBugNameClick(bug : Bug){
 		bug.isClosed = !bug.isClosed;
 	}
+
+	onRemoveClosedClick(){
+		for(let index = this.bugs.length-1; index >=0 ; index--){
+			if (this.bugs[index].isClosed)
+				this.bugs.splice(index, 1);
+		}
+	}
+
+	getClosedCount(){
+		let closedCount = 0;
+		for(let index = 0, count = this.bugs.length; index < count; index++){
+			if (this.bugs[index].isClosed)
+				++closedCount;
+		}
+		return closedCount;
+	}
 }
