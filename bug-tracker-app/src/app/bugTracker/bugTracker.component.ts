b04d2ebfcb9c2448361d5beb8ea5869dec79fe6d
Magesh <tkmagesh77@gmail.com>
2018-04-08 15:01:45 +0530

    Using async await and ngOnInit

--- bug-tracker-app/src/app/bugTracker/bugTracker.component.ts
+++ bug-tracker-app/src/app/bugTracker/bugTracker.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Bug } from './models/Bug';
 import { BugOperationsService } from './services/BugOperations.service';
 import axios from 'axios';
@@ -9,19 +9,46 @@ import axios from 'axios';
 	selector : 'app-bug-tracker',
 	templateUrl : 'bugTracker.component.html'
 })
-export class BugTrackerComponent{
+export class BugTrackerComponent implements OnInit{
 	bugs : Bug[] = [];
-
-	/*bugOperations : BugOperationsService = null;
-
-	constructor(_bugOperations : BugOperationsService){
-		this.bugOperations = _bugOperations;
-	}*/
 
 	sortBugBy : string = 'name';
 	sortBugDescending : boolean = false;
 
+	constructor(private bugOperations : BugOperationsService){
+		
+	}
+
+	async ngOnInit(){
+		this. bugs = await this.bugOperations.getAll()
+	}
 	
+	onNewBugCreated(newBug:Bug){
+		this.bugs = [...this.bugs, newBug];
+	}
+
+	async onBugNameClick(bugToToggle : Bug){
+		let toggledBug = await this.bugOperations.toggle(bugToToggle);
+		this.bugs = this.bugs.map(bug => bug === bugToToggle ? toggledBug : bug);
+	}
+
+	onRemoveClosedClick(){
+		this.bugs
+			.filter(bug => bug.isClosed)
+			.forEach(async closedBug => {
+				let result = await this.bugOperations.remove(closedBug);
+				this.bugs = this.bugs.filter(bug => bug.id !== closedBug.id);
+			});
+		
+	}
+
+}
+/*
+export class BugTrackerComponent{
+	bugs : Bug[] = [];
+
+	sortBugBy : string = 'name';
+	sortBugDescending : boolean = false;
 
 	constructor(private bugOperations : BugOperationsService){
 		this.bugOperations
@@ -50,3 +77,4 @@ export class BugTrackerComponent{
 	}
 
 }
+*/
